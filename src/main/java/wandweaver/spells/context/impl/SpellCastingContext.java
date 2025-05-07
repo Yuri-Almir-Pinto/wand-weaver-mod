d2d35955e4e4ea6beca1c403c9aa6335439f1647
yuri-almir-pinto <yurialmirp@gmail.com>
2025-05-07 14:46:56 -0300

    Converted SpellCastingContext to record.

--- src/main/java/wandweaver/spells/context/impl/SpellCastingContext.java
+++ src/main/java/wandweaver/spells/context/impl/SpellCastingContext.java
@@ -5,73 +5,13 @@ import wandweaver.spells.context.ISpellCastingContext;
 import wandweaver.spells.context.state.ISacrificedItemsState;
 import wandweaver.spells.context.utilities.*;
 
-public class SpellCastingContext implements ISpellCastingContext {
-    private final ServerPlayerEntity player;
-    private final ISoundUtilities sound;
-    private final IInteractionUtilities interaction;
-    private final ITargetingUtilities targeting;
-    private final IBlockConversionUtilities blockConversion;
-    private final IItemConversionUtilities itemConversion;
-    private final IEntityUtilities entity;
-    private final ISacrificedItemsState sacrificedItems;
-
-    public SpellCastingContext(
-            ServerPlayerEntity player,
-            ISoundUtilities sound,
-            IInteractionUtilities interaction,
-            ITargetingUtilities targeting,
-            IBlockConversionUtilities blockConversion,
-            IItemConversionUtilities itemConversion,
-            IEntityUtilities entity,
-            ISacrificedItemsState sacrificedItems
-    ) {
-        this.player = player;
-        this.sound = sound;
-        this.interaction = interaction;
-        this.targeting = targeting;
-        this.blockConversion = blockConversion;
-        this.itemConversion = itemConversion;
-        this.entity = entity;
-        this.sacrificedItems = sacrificedItems;
-    }
-
-    @Override
-    public ServerPlayerEntity player() {
-        return player;
-    }
-
-    @Override
-    public ISoundUtilities sound() {
-        return sound;
-    }
-
-    @Override
-    public IInteractionUtilities interaction() {
-        return interaction;
-    }
-
-    @Override
-    public ITargetingUtilities targeting() {
-        return targeting;
-    }
-
-    @Override
-    public IBlockConversionUtilities blockConversion() {
-        return blockConversion;
-    }
-
-    @Override
-    public IItemConversionUtilities itemConversion() {
-        return itemConversion;
-    }
-
-    @Override
-    public IEntityUtilities entity() {
-        return entity;
-    }
-
-    @Override
-    public ISacrificedItemsState sacrificedItems() {
-        return sacrificedItems;
-    }
+public record SpellCastingContext(
+        ServerPlayerEntity player,
+        ISoundUtilities sound,
+        IInteractionUtilities interaction,
+        ITargetingUtilities targeting,
+        IBlockConversionUtilities blockConversion,
+        IItemConversionUtilities itemConversion,
+        IEntityUtilities entity,
+        ISacrificedItemsState sacrificedItems) implements ISpellCastingContext {
 }
